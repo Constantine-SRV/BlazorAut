e2604ff818426c2a414550d666dccc6a9c2ac127
Constantinw-SRV <c.leshkov@gmail.com>
2025-05-15 15:22:19 +0300

    debug

--- BlazorAut/wwwroot/js/site.js
+++ BlazorAut/wwwroot/js/site.js
@@ -1,16 +1,18 @@
-function setCookie(name, value, days, dotNetHelper) {
+﻿function setCookie(name, value, days, dotNetHelper) {
     var expires = "";
     if (days) {
         var date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/; samesite=strict; secure";
-
+    document.cookie = name + "=" + (value || "") + expires + "; path=/;  secure";
+    
+    console.log("[site.js] document.cookie после setCookie:", document.cookie);
     dotNetHelper.invokeMethodAsync('RefreshAuthenticationState');
 }
 function deleteCookie(name, dotNetHelper) {
-    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/; samesite=strict; secure';
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;  secure';
 
+    console.log("[site.js] document.cookie после deleteCookie:", document.cookie);
     dotNetHelper.invokeMethodAsync('RefreshAuthenticationState');
 }
